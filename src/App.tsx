4c29a7ad751ac07f2560c225b4af1ed0a610b202
Peer Hartmann <development@einfachelogik.de>
2020-02-13 22:07:26 +0100

    schritt 4: installation des Webpack-dev-servers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { message } from "./messages"
 import image from "./img/octocat.png"
-
+const style = {
+    width: "200px"
+}
 export function App() {
-    return <div><img src={image}></img></div>
+    return <div><img style={style} src={image}></img></div>
 }
