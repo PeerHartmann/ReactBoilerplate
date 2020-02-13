56ade1c47f6371cf88a889b80bc24c11070e1a2b
Peer Hartmann <development@einfachelogik.de>
2020-02-13 21:51:28 +0100

    Alte Dateien entfernen

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { message } from "./messages"
-import image from "./img/octocat.png"
-console.log(message);   
-const img = document.createElement('img');
-img.src = image;
-img.width = 500;
-window.document.getElementsByTagName('body')[0].appendChild(img);
